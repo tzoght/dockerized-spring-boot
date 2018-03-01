6bca33f7900393f3063973690b43ca425b8cd03d
Tony Zoght <tony@zoght.com>
2018-03-01 16:34:33 -0500

    initial commit

---
+++ src/main/java/com/zoght/spring/boot/sample/HealthController.java
@@ -0,0 +1,13 @@
+package com.zoght.spring.boot.sample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @RequestMapping("/health")
+    public String health() {
+        return "All systems nominal!";
+    }
+}
